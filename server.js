2110ffcd9eba62244fa8810027134a0c84990027
Anupmish1974 <mishraanoop204@gmail.com>
2026-08-18 13:54:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 const server = http.createServer(app);
 
 const io = new Server(server, {
